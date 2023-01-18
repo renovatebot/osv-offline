d7f3a4ed337e8fd6c2bb37498dcf95e919ec77e5
Johannes Feichtner <johannes@web-wack.at>
2023-01-18 23:14:17 +0100

    feat: add package name to PURL conversion

---
+++ packages/osv-offline-db/src/lib/purl-helper.spec.ts
@@ -0,0 +1,21 @@
+import { packageToPurl } from './purl-helper';
+import { Ecosystem } from './ecosystem';
+
+describe('lib/purl-helper', () => {
+  describe('test ecosystems', () => {
+    test.each`
+      ecosystem      | packageName                         | purl
+      ${'crates.io'} | ${'libpulse-binding'}               | ${'pkg:cargo/libpulse-binding'}
+      ${'Go'}        | ${'github.com/hashicorp/consul'}    | ${'pkg:golang/github.com/hashicorp/consul'}
+      ${'Maven'}     | ${'org.apache.struts:struts2-core'} | ${'pkg:maven/org.apache.struts/struts2-core'}
+      ${'npm'}       | ${'@hapi/hoek'}                     | ${'pkg:npm/%40hapi/hoek'}
+      ${'NuGet'}     | ${'Microsoft.ChakraCore'}           | ${'pkg:nuget/Microsoft.ChakraCore'}
+      ${'PyPI'}      | ${'django'}                         | ${'pkg:pypi/django'}
+      ${'RubyGems'}  | ${'rails-html-sanitizer'}           | ${'pkg:gem/rails-html-sanitizer'}
+    `('$ecosystem | $packageName', ({ ecosystem, packageName, purl }) => {
+      expect(packageToPurl(ecosystem as Ecosystem, packageName as string)).toBe(
+        purl
+      );
+    });
+  });
+});
