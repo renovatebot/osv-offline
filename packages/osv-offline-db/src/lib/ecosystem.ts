8a681bb7d67a4aca6c3d700f17db4538abe7c03a
Michael Kriese <michael.kriese@visualon.de>
2025-01-01 21:40:27 +0100

    chore: fix comment

--- packages/osv-offline-db/src/lib/ecosystem.ts
+++ packages/osv-offline-db/src/lib/ecosystem.ts
@@ -1,4 +1,4 @@
-https://github.com/google/osv.dev/blob/b8ec0096f93f062993dee57f8b902f257993be88/osv/ecosystems/_ecosystems.py#L38-L68
+// https://github.com/google/osv.dev/blob/b8ec0096f93f062993dee57f8b902f257993be88/osv/ecosystems/_ecosystems.py#L38-L68
 export const ecosystems = [
   'crates.io',
   'Go',
