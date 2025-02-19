776527b1a210a1f0f87a0cac31010278dd81565f
Michael Kriese <michael.kriese@visualon.de>
2025-02-19 09:43:53 +0100

    chore: lint fix

--- jest.config.ts
+++ jest.config.ts
@@ -1,15 +1,15 @@
-import type { JestConfigWithTsJest } from "ts-jest";
+import type { JestConfigWithTsJest } from 'ts-jest';
 
 const config: JestConfigWithTsJest = {
-  preset: "ts-jest",
-  testPathIgnorePatterns: ["dist"],
-  setupFilesAfterEnv: ["jest-extended/all", "expect-more-jest"],
+  preset: 'ts-jest',
+  testPathIgnorePatterns: ['dist'],
+  setupFilesAfterEnv: ['jest-extended/all', 'expect-more-jest'],
   testTimeout: 15000,
   transform: {
-    "^.+\\.tsx?$": [
-      "ts-jest",
+    '^.+\\.tsx?$': [
+      'ts-jest',
       {
-        tsconfig: "<rootDir>/tsconfig.jest.json",
+        tsconfig: '<rootDir>/tsconfig.jest.json',
       },
     ],
   },
