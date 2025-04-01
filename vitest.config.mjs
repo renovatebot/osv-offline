dba0b6d3dc00d05e4c59ca1ec18a7a1f79dc085e
Angel Mendez Cano <amendez1988@gmail.com>
2025-04-01 18:33:39 +0000

    refactor: remove jest references
    - remove unused jest references

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -3,9 +3,6 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     testTimeout: 15000,
-    typecheck: {
-      enabled: true,
-      tsconfig: 'tsconfig.jest.json',
-    },
+    hookTimeout: 15000,
   },
 });
