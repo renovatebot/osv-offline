e05c66ef8628cc5fb3a2849a73d0251ecb6434b7
Angel Mendez Cano <amendez1988@gmail.com>
2025-08-26 04:57:23 +0000

    chore: update vitest reporter

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -4,5 +4,10 @@ export default defineConfig({
   test: {
     testTimeout: 15000,
     hookTimeout: 15000,
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json-summary', 'json'],
+      reportOnFailure: true,
+    },
   },
 });
