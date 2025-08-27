019528cd6936a48a2e2e2c51942c73ae73ca1693
Angel Mendez Cano <amendez1988@gmail.com>
2025-08-27 13:08:52 +0000

    feat: add vitest reporters

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -4,6 +4,7 @@ export default defineConfig({
   test: {
     testTimeout: 15000,
     hookTimeout: 15000,
+    reporters: ['verbose', 'github-actions', 'json'],
     coverage: {
       provider: 'v8',
       reporter: ['text', 'json-summary', 'json'],
