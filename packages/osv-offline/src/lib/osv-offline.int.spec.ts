aae16da50650cba55830ce09007fc5d82a535836
Michael Kriese <michael.kriese@visualon.de>
2025-11-17 17:19:11 +0100

    test: simplify tests (#1354)

--- packages/osv-offline/src/lib/osv-offline.int.spec.ts
+++ packages/osv-offline/src/lib/osv-offline.int.spec.ts
@@ -1,23 +1,11 @@
-import { OsvOfflineDb } from '@renovatebot/osv-offline-db';
-import fs from 'fs-extra';
 import { OsvOffline } from './osv-offline';
-import { afterAll, beforeAll, describe, expect, it, vi } from 'vitest';
-import path from 'path';
-import { tmpdir } from 'os';
+import { beforeAll, describe, expect, it } from 'vitest';
 
 describe('packages/osv-offline/src/lib/osv-offline.int', () => {
   let osvOffline: OsvOffline;
-  let tempDirPath: string;
 
   beforeAll(async () => {
-    tempDirPath = await fs.mkdtemp(path.join(tmpdir(), 'osv-offline_'));
-    vi.spyOn(OsvOfflineDb, 'rootDirectory', 'get').mockReturnValue(tempDirPath);
-    await fs.remove(OsvOfflineDb.rootDirectory);
     osvOffline = await OsvOffline.create();
-  });
-
-  afterAll(async () => {
-    await fs.rm(tempDirPath, { force: true, recursive: true });
   });
 
   describe('create', () => {
--- vitest.config.mjs
+++ vitest.config.mjs
@@ -1,13 +1,19 @@
+import { env } from 'node:process';
 import { defineConfig } from 'vitest/config';
+
+const ci = !!env.CI;
 
 export default defineConfig({
   test: {
-    testTimeout: 15000,
-    hookTimeout: 15000,
-    reporters: ['verbose', 'github-actions', 'json'],
+    testTimeout: 60_000,
+    hookTimeout: 60_000,
+    reporters: ci ? ['verbose', 'github-actions', 'junit'] : ['default'],
     coverage: {
       provider: 'v8',
-      reporter: ['text', 'json-summary', 'json'],
+      reporter: ci
+        ? ['text-summary', 'json']
+        : ['text-summary', 'html', 'json'],
+      enabled: ci ? true : undefined,
       reportOnFailure: true,
     },
   },
