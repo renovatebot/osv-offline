b30faf6a73d46bebf507baca5f34d87b4518d326
Michael Kriese <michael.kriese@visualon.de>
2025-11-17 17:53:05 +0100

    test: add test

--- packages/osv-offline/src/lib/download.int.spec.ts
+++ packages/osv-offline/src/lib/download.int.spec.ts
@@ -2,16 +2,43 @@ import { OsvOfflineDb } from '@renovatebot/osv-offline-db';
 import fs from 'fs-extra';
 import path from 'path';
 import { tryDownloadDb } from './download';
-import { describe, expect, it } from 'vitest';
+import { afterAll, beforeEach, describe, expect, it } from 'vitest';
 
 describe('packages/osv-offline/src/lib/download.int', () => {
   describe('tryDownloadDb', () => {
+    beforeEach(() => {
+      delete process.env.OSV_OFFLINE_DISABLE_DOWNLOAD;
+    });
+    afterAll(async () => {
+      const zipFilePath = path.join(
+        OsvOfflineDb.rootDirectory,
+        'osv-offline.zip'
+      );
+      await fs.rm(zipFilePath);
+    });
+
     it('works', async () => {
       const result = await tryDownloadDb();
 
       expect(result.success).toBe(true);
       expect(fs.stat(OsvOfflineDb.rootDirectory)).toBeDefined();
       expect(fs.readdir(OsvOfflineDb.rootDirectory)).not.toEqual([]);
+    });
+
+    it('skips if it should skip', async () => {
+      process.env.OSV_OFFLINE_DISABLE_DOWNLOAD = 'true';
+      await fs.ensureDir(OsvOfflineDb.rootDirectory);
+      const zipFilePath = path.join(
+        OsvOfflineDb.rootDirectory,
+        'osv-offline.zip'
+      );
+      await fs.writeFile(zipFilePath, Buffer.from([]));
+
+      const result = await tryDownloadDb();
+      expect(result.success).toEqual(true);
+
+      const stat = await fs.stat(zipFilePath);
+      expect(stat.size).toBe(0);
     });
 
     it('skips download if less than 1 day old', async () => {
@@ -27,8 +54,6 @@ describe('packages/osv-offline/src/lib/download.int', () => {
       expect(result.success).toEqual(true);
       const stat = await fs.stat(zipFilePath);
       expect(stat.size).toBe(0);
-
-      await fs.rm(zipFilePath);
     });
   });
 });
